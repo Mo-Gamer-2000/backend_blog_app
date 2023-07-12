67513329e09b3815f8c0a6074bbfefd242c8514a
Moeez Abdul <moeezrajput2000@gmail.com>
2023-07-12 20:38:13 +0100

    Connected Node.js to Database - Studio3T

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db";
 
 dotenv.config();
+connectDB();
 const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server is Running... .. .");
+  res.send("Server is running...");
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(`Server is Running on PORT ${PORT}... .. .`)
-);
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
